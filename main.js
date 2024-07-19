64f19ee4fcb4e84925314f6442af588237216820
annieremeeva <annieremeeva@gmail.com>
2024-07-19 21:03:40 +0300

    made basic functionality

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const valueInput = document.querySelector('.value-input');
+
 const progressCircle = document.querySelector('.progress');
 const radius = progressCircle.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
-progressCircle.style.strokeDasharray= circumference;
+progressCircle.style.strokeDasharray = circumference;
+progressCircle.style.strokeDashoffset = circumference;
+
 function setProgress(percent) {
-  progressCircle.style.strokeDashoffset = circumference - (circumference * percent / 100)
+  progressCircle.style.strokeDashoffset = circumference - (circumference * percent / 100);
 }
 
 valueInput.addEventListener('keyup', (e) => {
   if (e.code === 'Enter') {
     setProgress(valueInput.value);
+    valueInput.value='';
   }
+})
+
+const animateToggle = document.querySelector('.animate-toggle');
+
+animateToggle.addEventListener('change', () => {
+  progressCircle.classList.toggle('animated')
+})
+
+const hideToggle = document.querySelector('.hide-toggle');
+
+hideToggle.addEventListener('change', () => {
+  document.querySelector('.progress-wrapper').classList.toggle('hidden');
 })
